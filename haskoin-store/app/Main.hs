2a34e176f3a486371a19726532947ed0c16785e1
Jean-Pierre Rupp <xenog@protonmail.com>
2018-01-22 00:15:35 +0000

    Command-line improvements

--- haskoin-store/app/Main.hs
+++ haskoin-store/app/Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE GADTs             #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 import           Control.Concurrent.NQE
 import           Control.Exception
@@ -9,6 +8,7 @@ import           Control.Monad.Logger
 import           Control.Monad.Trans
 import           Data.Aeson                  (ToJSON (..), Value (..), object,
                                               (.=))
+import           Data.Default
 import           Data.Maybe
 import           Data.Monoid
 import           Data.String.Conversions
@@ -22,6 +22,7 @@ import           Network.HTTP.Types
 import           Options.Applicative
 import           System.Directory
 import           System.FilePath
+import           System.IO.Unsafe
 import           Web.Scotty.Trans
 
 type StoreM = ActionT Except IO
@@ -59,6 +60,16 @@ instance ToJSON Except where
     toJSON ServerError = object ["error" .= String "You made me kill a unicorn"]
     toJSON (StringError s) = object ["error" .= s]
 
+instance Default Config where
+    def =
+        Config
+        { configDir = Just $ unsafePerformIO myDirectory
+        , configCache = Just 250000
+        , configBlocks = Just 200
+        , configPort = Just 3000
+        , configNetwork = Just "bitcoin"
+        }
+
 config :: Parser Config
 config =
     Config <$>
@@ -67,30 +78,40 @@ config =
              str
              (metavar "DIR" <> long "dir" <> short 'd' <>
               help
-                  "Directory to store blockchain data (default: system-dependent)")) <*>
+                  ("Directory to store blockchain data (default: " <>
+                   fromJust (configDir def) <>
+                   ")"))) <*>
     optional
         (option
              auto
              (metavar "COUNT" <> long "cache" <> short 'c' <>
               help
-                  "Number of entries in UTXO cache for faster synchronisation (default: 250'000)")) <*>
+                  ("Number of entries in UTXO cache for faster synchronisation (default: " <>
+                   show (fromJust (configCache def)) <>
+                   ")"))) <*>
     optional
         (option
              auto
              (metavar "BLOCKS" <> long "blocks" <> short 'b' <>
               help
-                  "Number of blocks to download per request to peer (default: 200)")) <*>
+                  ("Number of blocks to download per request to peer (default: " <>
+                   show (fromJust (configBlocks def)) <>
+                   ")"))) <*>
     optional
         (option
              auto
              (metavar "PORT" <> long "port" <> short 'p' <>
-              help "Port number (default: 3000)")) <*>
+              help
+                  ("Port number (default: " <> show (fromJust (configPort def)) <>
+                   ")"))) <*>
     optional
         (option
              str
              (metavar "NETWORK" <> long "network" <> short 'n' <>
               help
-                  "Network to use: \"bitcoin\", \"testnet\", \"regtest\" (default: \"bitcoin\")"))
+                  ("Network to use: bitcoin, testnet or regtest (default: " <>
+                   fromJust (configNetwork def) <>
+                   ")")))
 
 defHandler :: Except -> StoreM ()
 defHandler ServerError = json ServerError
@@ -101,21 +122,29 @@ maybeJSON :: ToJSON a => Maybe a -> StoreM ()
 maybeJSON Nothing  = raise NotFound
 maybeJSON (Just x) = json x
 
+myDirectory :: IO FilePath
+myDirectory = getAppUserDataDirectory "haskoin-store"
+
 main :: IO ()
 main =
-    execParser opts >>= \Config {..} -> do
-        case configNetwork of
-            Nothing -> setProdnet
-            Just "testnet" -> setTestnet
-            Just "regtest" -> setRegtest
-            Just "bitcoin" -> setProdnet
-            Just _ ->
-                error
-                    "NETWORK environment variable must be \"bitcoin\", \"testnet\" or \"regtest\""
-        let port = fromMaybe 3000 configPort
-            blocks = fromMaybe 200 configBlocks
-            cache = fromMaybe 250000 configCache
-        dir <- maybe (getAppUserDataDirectory "haskoin-store") return configDir
+    execParser opts >>= \conf' -> do
+        let conf =
+                Config
+                    (configDir conf' <|> configDir def)
+                    (configCache conf' <|> configCache def)
+                    (configBlocks conf' <|> configBlocks def)
+                    (configPort conf' <|> configPort def)
+                    (configNetwork conf' <|> configNetwork def)
+        let port = fromJust $ configPort conf
+            blocks = fromJust $ configBlocks conf
+            cache = fromJust $ configCache conf
+            dir = fromJust $ configDir conf
+            net = fromJust $ configNetwork conf
+        case net of
+            "testnet" -> setTestnet
+            "regtest" -> setRegtest
+            "bitcoin" -> setProdnet
+            _ -> error "Network must be \"bitcoin\", \"testnet\" or \"regtest\""
         b <- Inbox <$> liftIO newTQueueIO
         s <- Inbox <$> liftIO newTQueueIO
         supervisor KillAll s [runWeb port b, runStore cache blocks dir b]
