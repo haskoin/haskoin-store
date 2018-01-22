839f86225aef819f48ac514308af66495ccfd242
Philippe Laprade <plaprade@gmail.com>
2018-01-22 20:39:15 +0000

    Add more tests and simplify import statements

--- haskoin-core/src/Network/Haskoin/Test/Transaction.hs
+++ haskoin-core/src/Network/Haskoin/Test/Transaction.hs
@@ -98,6 +98,17 @@ arbitraryAddrOnlyTxFull = do
     t    <- arbitrary
     return $ Tx v inps outs [] t
 
+-- | Like `arbitraryAddrOnlyTx` without empty signatures in the inputs
+arbitraryAddrOnlyTxFull :: Gen Tx
+arbitraryAddrOnlyTxFull = do
+    v    <- arbitrary
+    ni   <- choose (0,5)
+    no   <- choose (0,5)
+    inps <- vectorOf ni arbitraryAddrOnlyTxInFull
+    outs <- vectorOf no arbitraryAddrOnlyTxOut
+    t    <- arbitrary
+    return $ Tx v inps outs t
+
 -- | Arbitrary TxIn that can only be of type SpendPKHash or
 -- SpendScriptHash (multisig). Only compressed public keys are used.
 arbitraryAddrOnlyTxIn :: Gen TxIn
