312331225a4a7aeff58c368f9bfa73e13f95e6bc
Philippe Laprade <plaprade@gmail.com>
2018-02-10 13:19:52 +0000

    Add tests for best block height

--- haskoin-wallet/test/Network/Haskoin/Wallet/Spec.hs
+++ haskoin-wallet/test/Network/Haskoin/Wallet/Spec.hs
@@ -695,6 +695,9 @@ blockchainServiceSpec =
                     "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
             res <- httpTx blockchainInfoService tid
             txHash res `shouldBe` tid
+        it "can get the best block height (online test)" $ do
+            res <- httpBestHeight blockchainInfoService
+            res `shouldSatisfy` (> 500000)
 
 insightServiceSpec :: Spec
 insightServiceSpec =
@@ -732,6 +735,9 @@ insightServiceSpec =
                     "0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098"
             res <- httpTx insightService tid
             txHash res `shouldBe` tid
+        it "can get the best block height (online test)" $ do
+            res <- httpBestHeight insightService
+            res `shouldSatisfy` (> 500000)
 
 haskoinServiceSpec :: Spec
 haskoinServiceSpec =
@@ -770,6 +776,9 @@ haskoinServiceSpec =
                     "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
             res <- httpTx haskoinService tid
             txHash res `shouldBe` tid
+        it "can get the best block height (online test)" $ do
+            res <- httpBestHeight haskoinService
+            res `shouldSatisfy` (> 500000)
 
 {- Test Constants -}
 
