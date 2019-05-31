0fda24798bc88c05a64523b0f61ae29473537e06
orestis <orestis@blockchain.com>
2019-05-31 11:48:04 +0100

    TxHash unit test

--- test/Network/Haskoin/Store/DataSpec.hs
+++ test/Network/Haskoin/Store/DataSpec.hs
@@ -18,12 +18,13 @@ import           Test.Hspec
 
 spec :: Spec
 spec = do
-    let net = btcTest
-        def = TxHash "1111111111"
-    describe "Test BinDeserial" $
-          it "serializes tx" $ do
-            let tx = TxHash "0666939fb16533c8e5ebaf6052bb8c90d27ee53fe6035bb763de5253e0b1cd44"
-                raw = runPut $ binSerial net tx
-                deserial = runGet (binDeserial net) raw
-            (fromRight def deserial) `shouldBe` tx
+  let net = btcTest
+      def = TxHash "75ad09e40a8246beed18646c393b8687f084a23f1336179349f6891b6028e974"
+  describe "Test TxHash serialization" $
+    it "serializes tx" $ do
+      let tx = TxHash "0666939fb16533c8e5ebaf6052bb8c90d27ee53fe6035bb763de5253e0b1cd44"
+          raw = runPut $ binSerial net tx
+          deserial = runGet (binDeserial net) raw
+      fromRight def deserial `shouldBe` tx
+
 
