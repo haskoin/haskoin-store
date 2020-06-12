653b9d8abf2770f7c9ce2d400a78bd664eff2f56
Jean-Pierre Rupp <jprupp@protonmail.ch>
2020-06-12 09:36:06 +0100

    Remove Paths_haskoin_store to make stack happy

--- store/src/Paths_haskoin_store.hs
+++
@@ -1,8 +0,0 @@
-module Paths_haskoin_store where
-
-import           Data.Version                 (Version, parseVersion)
-import           Text.ParserCombinators.ReadP (readP_to_S)
-
-
-version :: Version
-version = fst (head (readP_to_S parseVersion "0.0.0"))
